05e09dbc55d98433b1f79d685d687d6f2f29716d
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-02-21 15:31:12 +0100

    Delete Manipulation_d'un_dictionnaire.py

--- Manipulation_d'un_dictionnaire.py
+++
@@ -1,8 +0,0 @@
-"""Manipulation d'un dictionnaire"""
-
-performances = {'bob' : 50, 'jean' : 30, 'marie' : 45, 'kylian' : 2}
-performances['marie'] += 2
-performances['julie'] = -5
-x=len(performances)
-print(x)
-print(performances)
