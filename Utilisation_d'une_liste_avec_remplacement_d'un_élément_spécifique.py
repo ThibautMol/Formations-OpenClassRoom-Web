75fd13cbfb1fe13573fc5d6479c58edb52f1bb8a
Thibaut Molinié <123956618+ThibautMol@users.noreply.github.com>
2023-02-21 15:31:33 +0100

    Delete Utilisation_d'une_liste_avec_remplacement_d'un_élément_spécifique.py

--- Utilisation_d'une_liste_avec_remplacement_d'un_élément_spécifique.py
+++
@@ -1,7 +0,0 @@
-"""Utilisation d'une liste avec remplacement d'un élément spécifique"""
-
-colors = ['red', 'yellow', 'orange', 'green', 'blue']
-colors[-2]="emerald"
-print(colors)
-for color in colors :
-    print(color)
